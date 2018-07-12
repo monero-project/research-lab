591a498cfbe8e7d93bcc281b1eef83fcc0090202
SarangNoether <32460187+SarangNoether@users.noreply.github.com>
2018-07-12 17:38:16 -0400

    Initial draft of Dumb25519 library

---
+++ source-code/rupol/dumb25519.py
@@ -0,0 +1,150 @@
+# Dumb25519: a stupid implementation of ed25519
+#
+# Use this code only for prototyping
+# -- putting this code into production would be dumb
+# -- assuming this code is secure would also be dumb
+
+import random
+import hashlib
+
+# curve parameters
+b = 256
+q = 2**255 - 19
+l = 2**252 + 27742317777372353535851937790883648493
+
+# Useful helper methods
+def exponent(b,e,m):
+    return pow(b,e,m)
+
+def invert(x):
+    return exponent(x,q-2,q)
+
+def xfromy(y):
+    temp = (y*y-1) * invert(d*y*y+1)
+    x = exponent(temp,(q+3)/8,q)
+    if (x*x - temp) % q != 0:
+        x = (x*I) % q
+    if x % 2 != 0:
+        x = q-x
+    return x
+
+d = -121665 * invert(121666)
+I = exponent(2,(q-1)/4,q)
+
+class Scalar:
+    def __init__(self,x):
+        self.x = x % l
+
+    def __add__(self,y):
+        return Scalar(self.x + y)
+
+    def __sub__(self,y):
+        return Scalar(self.x - y)
+
+    def __mul__(self,y):
+        return Scalar(self.x*y)
+
+    def __div__(self,y):
+        return Scalar(int(self.x/y.x))
+
+    def __eq__(self,y):
+        return self.x == y.x
+
+    def __ne__(self,y):
+        return self.x != y.x
+
+class Point:
+    def __init__(self,x,y):
+        self.x = x
+        self.y = y
+
+    def __eq__(self,Q):
+        return self.x == Q.x and self.y == Q.y
+
+    def __ne__(self,Q):
+        return self.x != Q.x or self.y != Q.y
+
+    def __add__(self,Q):
+        x1 = self.x
+        y1 = self.y
+        x2 = Q.x
+        y2 = Q.y
+        x3 = (x1*y2+x2*y1) * invert(1+d*x1*x2*y1*y2)
+        y3 = (y1*y2+x1*x2) * invert(1-d*x1*x2*y1*y2)
+        return Point(x3 % q, y3 % q)
+
+    def __sub__(self,Q):
+        x1 = self.x
+        y1 = self.y
+        x2 = -Q.x
+        y2 = Q.y
+        x3 = (x1*y2+x2*y1) * invert(1+d*x1*x2*y1*y2)
+        y3 = (y1*y2+x1*x2) * invert(1-d*x1*x2*y1*y2)
+        return Point(x3 % q, y3 % q)
+
+    def __mul__(self,y):
+        if y == Scalar(0):
+            return Point(0,1)
+        Q = self.__mul__(y/Scalar(2))
+        Q = Q.__add__(Q)
+        if y.x & 1:
+            Q = self.__add__(Q)
+        return Q
+
+    def on_curve(self):
+        x = self.x
+        y = self.y
+        return (-x*x + y*y - 1 - d*x*x*y*y) % q == 0
+
+# common basepoint
+Gy = 4*invert(5)
+Gx = xfromy(Gy)
+G = Point(Gx % q, Gy % q)
+
+# zero point
+Z = Point(0,1)
+
+# make a point from a given integer y (if it is on the curve)
+def make_point(y):
+    x = xfromy(y)
+    P = Point(x,y)
+    if not P.on_curve():
+        return None
+    return P
+
+# hash data to get a point on the curve in the G subgroup
+def hash_to_point(data):
+    while True:
+        data = hashlib.sha256(data).hexdigest()
+        if make_point(int(data,16)) is not None:
+            return make_point(int(data,16))*Scalar(8)
+
+# hash data to get a scalar
+def hash_to_scalar(data):
+    return Scalar(int(hashlib.sha256(data).hexdigest(),16))
+
+# generate a random scalar
+def random_scalar():
+    return Scalar(random.randrange(0,l))
+
+# generate a random point in the G subgroup
+def random_point():
+    return hash_to_point(str(random.random()))
+
+#############
+# TEST RUNS #
+#############
+print 'Running tests...'
+
+assert Z == G*Scalar(0)
+assert G == G*Scalar(1)
+assert G+G == G*Scalar(2)
+assert Z == G + G*Scalar(-1)
+assert G-G == Z
+assert G-(G*Scalar(-1)) == G+G
+test1 = G*hash_to_scalar('This is a test')
+test2 = hash_to_point('This is another test')
+test3 = hash_to_point('Four score and seven hashes ago...')
+test4 = random_point()
+
+print 'Tests complete!'
