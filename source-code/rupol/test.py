7730ef823cbee7df19e17ec075f58670be7b5bd9
SarangNoether <32460187+SarangNoether@users.noreply.github.com>
2018-07-12 21:49:59 -0400

    Added tests for Dumb25519 library

---
+++ source-code/rupol/test.py
@@ -0,0 +1,71 @@
+# Test suite for Dumb25519 and friends
+
+from dumb25519 import *
+import unittest
+
+class TestDumb25519(unittest.TestCase):
+    def test_point_operations(self):
+        self.assertEqual(Z+Z,Z)
+        self.assertEqual(G+Z,G)
+        self.assertEqual(Z+G,G)
+        self.assertEqual(G-G,Z)
+        self.assertFalse(Point(0,0).on_curve())
+        self.assertTrue(G.on_curve())
+        self.assertTrue(Z.on_curve())
+    
+    def test_scalar_operations(self):
+        self.assertEqual(Scalar(0),Scalar(l))
+        self.assertEqual(Scalar(0)+Scalar(1),Scalar(1))
+        self.assertEqual(Scalar(0)-Scalar(1),Scalar(-1))
+        self.assertEqual(Scalar(1)*Scalar(1),Scalar(1))
+        self.assertEqual(Scalar(2)/Scalar(2),Scalar(1))
+        self.assertEqual(Scalar(3)/Scalar(2),Scalar(1))
+        self.assertEqual(Scalar(0)/Scalar(2),Scalar(0))
+
+    def test_mixed_operations(self):
+        self.assertEqual(G*Scalar(0),Z)
+        self.assertEqual(G*Scalar(1),G)
+        self.assertEqual(G*Scalar(2),G+G)
+        self.assertEqual(G+G*Scalar(-1),Z)
+        self.assertEqual(G-G*Scalar(-1),G+G)
+
+        with self.assertRaises(TypeError):
+            G+Scalar(1)
+        with self.assertRaises(TypeError):
+            G-Scalar(1)
+        with self.assertRaises(TypeError):
+            G*Z
+        with self.assertRaises(TypeError):
+            Scalar(1)+G
+        with self.assertRaises(TypeError):
+            Scalar(1)-G
+        with self.assertRaises(TypeError):
+            Scalar(1)*G
+        with self.assertRaises(TypeError):
+            Scalar(1)/G
+        with self.assertRaises(TypeError):
+            G == Scalar(1)
+        with self.assertRaises(TypeError):
+            Scalar(1) == G
+
+    def test_hashing(self):
+        hash_to_point('The Human Fund: Money For People')
+        hash_to_scalar('The Human Fund: Money For People')
+        with self.assertRaises(TypeError):
+            hash_to_point(8675309)
+        hash_to_point(str(8675309))
+        with self.assertRaises(TypeError):
+            hash_to_scalar(8675309)
+        hash_to_scalar(str(8675309))
+        with self.assertRaises(TypeError):
+            hash_to_point(G)
+        hash_to_point(str(G))
+        with self.assertRaises(TypeError):
+            hash_to_scalar(G)
+        hash_to_scalar(str(G))
+
+    def test_random(self):
+        random_scalar()
+        random_point()
+
+unittest.TextTestRunner(verbosity=2).run(unittest.TestLoader().loadTestsFromTestCase(TestDumb25519))
